897e8c927a2f6c0106b867053501ab7c3c580d10
cbartram <cbartram3@gmail.com>
2020-04-24 12:55:13 -0400

    Added mongodb

--- src/main/java/com/healthkit/consumer/HealthConsumer/HealthConsumerApplication.java
+++ src/main/java/com/healthkit/consumer/HealthConsumer/HealthConsumerApplication.java
@@ -1,13 +1,23 @@
 package com.healthkit.consumer.HealthConsumer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Slf4j
 @SpringBootApplication
 public class HealthConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthConsumerApplication.class, args);
 	}
+}
 
+@Document
+class Reservation {
+	@Id
+	private String id;
+	private String name;
 }
--- src/main/java/com/healthkit/consumer/HealthConsumer/controller/IngestionController.java
+++
@@ -1,9 +0,0 @@
-package com.healthkit.consumer.HealthConsumer.controller;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class IngestionController {
-
-}
