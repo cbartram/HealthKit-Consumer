d324d6f23ef5cc431bc2b52552ddb0765f986abf
cbartram <cbartram3@gmail.com>
2020-05-04 16:54:50 -0400

    Added fetching on an interval

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
 import './App.css';
 import LineGraph from "./components/LineGraph/LineGraph";
+import {fetchHealthMetrics} from "./actions/actions";
 
 const mapStateToProps = state => ({
-  data: state.health.metrics.map((m, i) => ({ value: parseFloat(m.value), timestamp: moment().subtract(i, 'minutes').format('hh:mm') })),
+    data: state.health.metrics.map((m, i) => ({ value: parseFloat(m.value), timestamp: moment().subtract(i, 'minutes').format('hh:mm') })),
+});
+
+const mapDispatchToProps = dispatch => ({
+    fetchHeartRate: () => dispatch(fetchHealthMetrics())
 });
 
 function App(props) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          View latest Health Metrics
-        </p>
-        <LineGraph series={[{ name: 'BPM', data: props.data }]} />
-      </header>
-    </div>
-  );
+
+    const [healthMetrics, setHealthMetrics] = useState([]);
+
+    useEffect(() => {
+        const interval = setInterval(async () => {
+            await props.fetchHeartRate();
+            setHealthMetrics([{ name: 'BPM', data: props.data }])
+        }, 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    View latest Health Metrics
+                </p>
+                <LineGraph series={healthMetrics} />
+            </header>
+        </div>
+    );
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
