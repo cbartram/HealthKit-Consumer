49a9537646b5cb97b85db312ebb28ff12458dbe1
cbartram <cbartram3@gmail.com>
2020-05-10 13:22:21 -0400

    Added trigger border

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu } from 'semantic-ui-react'
+import { history} from "../../util/util";
 import Logo from '../../resources/images/logo.png';
+import { useAuth0 } from "../../util/auth0-spa";
 import './Navbar.css';
 
-
-const Navbar = (props) => {
-  return (
+const Navbar = () => {
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+    const [activePage, setActivePage] = useState();
+    return (
       <Menu>
           <Menu.Item>
               <img src={Logo} alt="logo" />
           </Menu.Item>
-          <Menu.Item name='home' active>
+         <Menu.Item name='home' active={activePage === 'home'} onClick={() => {
+             history.push('/');
+             setActivePage('home');
+         }}>
           Home
         </Menu.Item>
-        <Menu.Item name='metrics'>
-          Metrics
-        </Menu.Item>
-        <Menu.Item name='analytics'>
-          Analytics
-        </Menu.Item>
+          {
+              isAuthenticated ?
+              <Menu.Item
+                  active={activePage === 'dashboard'}
+                  onClick={() =>  {
+                      history.push('/dashboard');
+                      setActivePage('dashboard');
+                  }}>
+                  Dashboard
+              </Menu.Item> :
+              <Menu.Item
+                  onClick={() => loginWithRedirect({})}
+              >
+                  Login
+              </Menu.Item>
+          }
+          {
+              isAuthenticated &&
+                <Menu.Item onClick={() => logout()}>
+                  Logout
+                </Menu.Item>
+          }
       </Menu>
   )
 };
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
+import { history } from '../../util/util';
 import withContainer from "../../components/withContainer";
 import './Home.css';
-import { Link } from "react-router-dom";
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <h1>Home Page</h1>
-                <Button primary><Link to="/dashboard">View Metrics</Link></Button>
+                <Button primary onClick={() => history.push('/dashboard')}>View Metrics</Button>
             </div>
         )
     }
