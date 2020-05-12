3884ed3a6d64e300abf08680476d8490b3c15653
cbartram <cbartram3@gmail.com>
2020-05-12 17:45:46 -0400

    Added properties files for preprod and prod and removed basic auth on useless endpoints

--- src/main/java/com/healthkit/consumer/HealthConsumer/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/healthkit/consumer/HealthConsumer/configuration/security/SecurityConfiguration.java
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -45,7 +42,6 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-
     /**
      * Sets Basic Http Auth when attempting to retrieve an access token
      * through the OAuth server endpoint i.e all paths that have /oauth/** in them.
@@ -58,31 +54,11 @@ public class SecurityConfiguration {
                     exchanges
                         .pathMatchers(HttpMethod.OPTIONS).permitAll()
                         .pathMatchers("/ws/**").permitAll()
-                        .pathMatchers("/oauth/**")
-                            .authenticated().and().httpBasic()
                         .and()
                         .authorizeExchange()
                         .anyExchange().authenticated()
-
                 )
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
                 .build();
-    }
-
-    /**
-     * Configures the username and password that is
-     * used for Basic Auth on the /oauth/token endpoint
-     * @param username String username injected by Spring Application context
-     * @param password String password set it properties and injected by spring
-     * @return MapReactiveUserDetailsService Object
-     */
-    @Bean
-    public MapReactiveUserDetailsService userDetailsService(@Value("${spring.security.username}") final String username, @Value("${spring.security.pass}") final String password) {
-        UserDetails user = User
-                .withUsername(username)
-                .password(passwordEncoder().encode("{noop}" + password))
-                .roles("USER")
-                .build();
-        return new MapReactiveUserDetailsService(user);
     }
 }
